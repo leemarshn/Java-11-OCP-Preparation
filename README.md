7ae6def62fdcc7b3f602ec6c929f4e6ca18e628e
Lee Ndegwa <leendegwa@gmail.com>
2021-03-07 16:20:31 +0300

    update read me

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
 # Java-11-OCP-Preparation in 60 Days
+
+## Basics
+
+* -d parameter points to a path to store compilation result
+* the -classpath or -cp parameters points to locations of other classes that may be required to compile your code
+
+* to compile use - javac 
+
+`javac -cp /prjects/classes -d  /project/classes /project/whatever.java`
+
+* to execute use - java
+
+`java -cp /project/classes demo.Whatever Jo John "A Name" Hello John`
+
+* in Java 11 you can use single file source code; that means to can execute the code directly without compiling, that is not good for production
+
+`java /project/sources/demo/Whatever.java`
+
+##### Comments
+* For comments javadoc ignores the multi-line and single-line comments
+* Documentation comments ->  /** May contain html markup. descriptive tags prefixed with @sign and are used by *javadoc* tool to generate documentation
+
+`javadoc -d <documentation path> -sourcepath <source code epath> -subpackages <>name of the root package ` 
+
+# Java Primitives
+* Java has 8 primitive types
+* primitives must be initialized before use
+##### whole Numbers
+* byte - bits -128 to 127
+* short - 16 bits -32,768 to 32,767
+* int - 32 bits - -2m
+* long - 64 bits
+
+###### whole numbers can be represented into
+
+* Binary - ob10001
+* octal - 072
+* Decimal - 1234
+* Hex - 0x4f
+
+##### floating point
+* double - 64 bits
+* float - 32 bits use *F* to distinguish from double
+* you can use exponential *E* for floating points
+
+##### boolean
+* default is false
+* you can use *true* or *false* to initialize boolean, those keywords should be always in lowercase
+
+##### char
+* initialized using single quote *'*
+* cannot contain more than one character
+
+#### Method Overloading
+
+* two ``or more methods within the same class that have identical names
+* must have identical return types
+* Must have a different number or different types of parameters or both
+
+* Constructors invocation is not  allowed, if one constructor calls another constructor the other constructor should not call it.
+
+#### Enum
+
+* Enumeration provides a fixed set of instances of a specific type
+* enums are implicitly public static and final
+* instances are of type enum
+* you can use `static import Condition.*`
+* a constructor should be added to an enum to initialize its instance variables
+* the constructor must be default or private
+* declaration of enum values invokes enum constructor
+
+        `public enum Condition{
+            HOT, WARM, COLD;
+        }`
+        
+#### Java Memory Allocation
+
+* has the stack and heap
+* Stack is a memory context of a thread, storing local method variable
+* Heap is a shared memory area, accessible from different methods and thread contexts
+* stack only holds primitive and object references.
+* Classes and objects are stored in the heap
+
+
+## Inheritance
+#### casting
+* an object can be referenced using either specific child subclass type or generic parent superclass
+* casting is required to assign parent to child reference type.
+* Casting is not required to assign child to parent
+* Casting is not possible between objects of type siblings
+
+#### instanceof
+
+* returns true or false
+        
+   
--- src/app/Shop.java
+++ src/app/Shop.java
@@ -1,6 +1,10 @@
-package com.company.productManagement;
+package app;
+
+import com.company.data.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 /**
  * @author LeeN
@@ -10,33 +14,34 @@ import java.math.BigDecimal;
 
 public class Shop {
     public static void main(String[] args) {
-        Product p1 = new Product("312FDS21", "MACINTOSH", BigDecimal.valueOf(1.99));
-        Product p2 = new Product("DFSKJ23K", "toshiba", BigDecimal.valueOf(0.99), Rating.FIVE_STAR);
-        Product p5 = p2.applyRating(Rating.FOUR_STAR);
+
+        ProductManager pm = new ProductManager();
+
+        Product p1 = pm.createProduct("1012","Tea", BigDecimal.valueOf(1.99), Rating.THREE_STAR);
+        Product p2 = pm.createProduct("DFSKJ23K", "Coffee", BigDecimal.valueOf(300), Rating.FOUR_STAR);
+        Product p3 = pm.createProduct("30383088", "Bread", BigDecimal.valueOf(50),Rating.FIVE_STAR, LocalDate.now().plusDays(2));
+        Product p4 = pm.createProduct("104", "Chocolate", BigDecimal.valueOf(45), Rating.FIVE_STAR);
+        Product p5 = pm.createProduct("104", "Chocolate", BigDecimal.valueOf(45), Rating.FOUR_STAR, LocalDate.now().plusDays(8));
+
+//        if (p3 instanceof Food){
+//            LocalDate bestBefore = ((Food)p3).getBestBefore();
+//        }
+//        Product p5 = p2.applyRating(Rating.FOUR_STAR);
+        //p3.getBestBefore();
 
 
-        System.out.println(
-                " Product ID: " + p1.getId()
-                        + " Product Name: " + p1.getName()
-                        + " Price: " + p1.getPrice()
-                        + " Discount: " + p1.getDiscountRate()
-                        + " Rating: " + p1.getRating().getStars());
+//        System.out.println(" Product ID: " + p1.getId() + " - Product Name: " + p1.getName() + " - Price: " + p1.getPrice() + " - Discount: " + p1.getDiscountRate() + " - Rating: " + p1.getRating().getStars());
+//        System.out.println(" Product ID: " + p2.getId() + " - Product Name: " + p2.getName() + " - Price: " + p2.getPrice() + " - Discount: " + p2.getDiscountRate() + " - Rating: " + p2.getRating().getStars());
+//        System.out.println(" Product ID: " + p3.getId() + " - Product Name: " + p3.getName() + " - Price: " + p3.getPrice() + " - Discount: " + p3.getDiscountRate() + " - Rating: " + p3.getRating().getStars() + " - Best Before: " );
+//      //  System.out.println(" Product ID: " + p2.getId() + " Product Name: " + p2.getName() + " Price: " + p2.getPrice() + " Discount: " + p2.getDiscountRate() + " Rating: " + p2.getRating().getStars());
+//        System.out.println(" Product ID: " + p5.getId() + " - Product Name: " + p5.getName() + " - Price: " + p5.getPrice() + " - Discount: " + p5.getDiscountRate() + " - Rating: " + p5.getRating().getStars());
+//
 
-
-        System.out.println(
-                " Product ID: " + p2.getId()
-                        + " Product Name: " + p2.getName()
-                        + " Price: " + p2.getPrice()
-                        + " Discount: " + p2.getDiscountRate()
-                        + " Rating: " + p2.getRating().getStars());
-        System.out.println(
-                " Product ID: " + p5.getId()
-                        + " Product Name: " + p5.getName()
-                        + " Price: " + p5.getPrice()
-                        + " Discount: " + p5.getDiscountRate()
-                        + " Rating: " + p5.getRating().getStars());
-
-
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4);
+        System.out.println(p5);
 
     }
 }
